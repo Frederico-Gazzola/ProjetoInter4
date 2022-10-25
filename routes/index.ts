f01cb15a2ecc2de0a5c904caaa31773f91e6108a
Frederico Gazzola Braga <fredoncii@hotmail.com>
2022-10-25 09:48:01 -0300

    Primeira versao

---
+++ routes/index.ts
@@ -0,0 +1,37 @@
+﻿import app = require("teem");
+
+//**********************************************************************************
+// Se por acaso ocorrer algum problema de conexão, autenticação com o MySQL,
+// por favor, execute este código abaixo no MySQL e tente novamente!
+//
+// ALTER USER 'USUÁRIO'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SENHA';
+//
+// * Assumindo que o usuário seja root e a senha root, o comando ficaria assim:
+//
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+//
+//**********************************************************************************
+
+class IndexRoute {
+	public async index(req: app.Request, res: app.Response) {
+		// Mais para frente iremos melhorar os tipos, para não usar any[] :)
+		/*let pessoas: any[];
+
+		await app.sql.connect(async (sql) => {
+
+			// Todas os comandos SQL devem ser executados aqui dentro do app.sql.connect().
+
+			pessoas = await sql.query("SELECT id, nome, email FROM pessoa");
+
+		});
+
+		let opcoes = {
+			pessoas: pessoas
+		};*/
+
+		//res.render("index/index", opcoes);
+		res.render("index/index");
+	}
+}
+
+export = IndexRoute;
